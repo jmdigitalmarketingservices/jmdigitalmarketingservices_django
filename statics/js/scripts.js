d73f32a48351985f38c779ccf82ce451763939fb
Fahim Al Islam <fahim6668@gmail.com>
2021-03-05 03:46:01 +0600

    Changed: added submit form

--- statics/js/scripts.js
+++ statics/js/scripts.js
@@ -10,8 +10,8 @@ $("#submitButton").on("click", function () {
     const email = $("#email").val();
     const phone = $("#phone").val();
     const website = $("#website").val();
-    const username = "";
-    const password = ""
+    const username = "57c0ef882a9d275b73efd242f60b2458";
+    const password = "6b452c1d00684460267e8c4e0e0c16a5"
     if (name === '' || email === '' || phone === '') {
         alert("Please Fill Required Fields");
     } else {
@@ -23,7 +23,7 @@ $("#submitButton").on("click", function () {
                 "Authorization": "Basic " + btoa(username + ":" + password),
                 "Content-Type": "application/json"
             },
-            data: {
+            data: JSON.stringify({
                 "Messages": [
                     {
                         "From": {
@@ -41,7 +41,7 @@ $("#submitButton").on("click", function () {
                         "HTMLPart": "Email: " + email + ", Name: " + name + ", Phone: " + phone + ", website: " + website
                     }
                 ]
-            },
+            }),
             success: function () {
                 $("#form")[0].reset();
                 alert('Thanks for your response!');
