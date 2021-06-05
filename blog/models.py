6bd079f7502b506e6826d9ff03d65c8c99517e2e
Fahim Al Islam <fahim@marvelous-tech.com>
2021-06-06 02:38:25 +0600

    Updated meta tags and deleted data

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,9 @@ class Blog(models.Model):
     slug = models.SlugField(blank=True, null=True, max_length=255)
     is_active = models.BooleanField(default=False)
 
+    seo_title = models.CharField(max_length=70, null=True, blank=True)
+    seo_description = models.TextField(null=True, blank=True)
+
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
--- blog/views.py
+++ blog/views.py
@@ -54,11 +54,33 @@ def view(request):
 def details(request, slug):
     queryset = Blog.objects.filter(is_active=True)
     blog = get_object_or_404(queryset, slug=slug)
+    meta = Meta(
+        title=blog.seo_title,
+        og_title=blog.seo_title,
+        twitter_title=blog.seo_title,
+        schemaorg_title=blog.seo_title,
+        image=blog.image_url,
+        image_object={
+            'url': blog.image_url,
+            'secure_url': blog.image_url,
+            'type': 'image/jpeg',
+            'width': 1200,
+            'height': 630,
+            'alt': 'JM Digital Marketing Services Image',
+        },
+        image_height=1230,
+        image_width=630,
+        object_type='Blog of J. M. Digital Marketing Services',
+        description=blog.seo_description,
+        site_name='JM Digital Marketing Services',
+        extra_custom_props=[
+            ('http-equiv', 'Content-Type', 'text/html; charset=UTF-8'),
+        ]
+    )
     context = {
         'blog': blog,
         'has_contact': True,
         'contact_header': 'GET A FREE SITE AUDIT',
-        'title': 'SEO Digital Marketing Services Agency Toronto 📱 | J. M. Digital Marketing Services 🥇',
-        'meta_description': 'Toronto&#039;s Best SEO Digital Marketing Agency Service for page #1🥇 ranking! Call 647-990-2414. FREE WEB ANALYSIS. Economical and affordable SEO and Social Media Services.'
+        'meta': meta
     }
     return render(request, 'blog/details/index.html', context)
